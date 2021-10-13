98cc6b3ebf55cfec378fdbdc12cc27ad4a0698b4
edilbertojj65 <edilbertojj65@gmail.com>
2021-10-13 12:20:59 -0400

    Deploy

--- app/serializers/photo_serializer.rb
+++ app/serializers/photo_serializer.rb
@@ -1,4 +1,4 @@
 class PhotoSerializer < ActiveModel::Serializer
-  attributes :id, :namePhoto, :url, :fileName
+  attributes :id, :namePhoto, :url, :fileName, :user_id, :group_user_id
   has_many   :review
 end
